1df244aab7a5fe99f8b51a01bebf8522260589ff
BasselTharwat <bassel.tharwat01@gmail.com>
2025-12-07 01:45:54 +0200

    done with ch 13

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,5 +1,5 @@
 import DashboardSkeleton from "../../ui/skeletons";
 
-export default function(){
-    return <DashboardSkeleton />;
+export default function Loading() {
+  return <DashboardSkeleton />;
 }
